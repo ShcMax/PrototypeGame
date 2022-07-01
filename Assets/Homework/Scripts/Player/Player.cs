ec82423c217d089cdaa678e420f7f7da413d2249
ShcMax <102209342+ShcMax@users.noreply.github.com>
2022-07-01 19:04:05 +0300

    Day_3.2

--- Assets/Homework/Scripts/Player/Player.cs
+++ Assets/Homework/Scripts/Player/Player.cs
@@ -8,9 +8,17 @@ namespace Maze
     public class Player : Unit, ICloneable
     {
 
+        int x = 10;
+        int y = 20;
+
+        string s1 = "Temp";
+        string s2 = "Test";
+
         public override void Awake()
         {
             base.Awake();
+            Swap(ref x, ref y);
+            Swap(ref s1, ref s2); 
         }
 
         public override void Move(float x, float y, float z)
@@ -33,5 +41,11 @@ namespace Maze
         //        _rigidbody.AddForce(Vector3.up * _forceJump, ForceMode.Force);
         //    }
         //}
+        void Swap<T>(ref T x, ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
     }
 }
